7bf9f27cb9e370e48cbc4961e12b5bfc04389111
Erica LaBuono <erica.labuono@gmail.com>
2022-02-13 20:39:26 -0500

    Mood checks now functional
    Mood checkboxes now functional and sample data added to CSV from test

--- polarity.py
+++ polarity.py
@@ -7,49 +7,68 @@ database = '2022.csv'
 class Window:
     def __init__(self, master):
         self.master = master
-        self.color = StringVar(value="test")
+        self.color = StringVar(value="BLANK")
+        self.manic = BooleanVar(value="False")
+        self.depressive = BooleanVar(value="False")
+        self.overwhelmed = BooleanVar(value="False")
+        self.apathetic = BooleanVar(value="False")
+        self.happy = BooleanVar(value="False")
+        self.sad = BooleanVar(value="False")
+        self.irritable = BooleanVar(value="False")
+        self.calm = BooleanVar(value="False")
+        self.empty = BooleanVar(value="False")
+        self.fulfilled = BooleanVar(value="False")
 
         # Store data
         def submit():
             with open(database, 'a') as csvfile:
                 csvwriter = csv.writer(csvfile)
                 row = [datetime.now().strftime("%m/%d/%Y %H:%M"),
-                        self.color.get(),'N','N','N','N','N','N','N','N','N','N']
+                        self.color.get(), self.manic.get(),
+                        self.depressive.get(), self.overwhelmed.get(),
+                        self.apathetic.get(), self.happy.get(),
+                        self.sad.get(), self.irritable.get(),
+                        self.calm.get(), self.empty.get(),
+                        self.fulfilled.get()]
                 csvwriter.writerow(row)
             self.master.destroy()
 
         # Frames
         self.frame = Frame(self.master, width = 300, height = 300)
         self.frame.pack( padx = 10, pady = 10, side = LEFT)
-        self.colorframe = Frame(self.frame, width = 250, height = 250)
-        self.colorframe.grid(row = 1, column = 0, padx = 10, pady = 10, sticky = SW)
+        self.colorframe = Frame(self.frame, width = 200, height = 100)
+        self.colorframe.grid(row = 1, column = 0, padx = 10, pady = 10)
+        self.moodframe = Frame(self.frame, width = 200, height = 200)
+        self.moodframe.grid(row = 2, column = 0, padx = 10, pady = 10)
 
         # Label
         self.label = Label(self.frame, text="  What color is your mood?  ", font = "Helvetica 16 italic", relief=GROOVE)
         self.label.grid(row=0, column=0,  padx = 10, pady = 10)
 
         # Radios/Colors
-        self.radiored = Radiobutton(self.colorframe, variable = self.color, value = "red", text = "RED")
-        self.radiored.grid(row = 1, column = 0, padx = 5, pady = 5)
-        self.radioorange = Radiobutton(self.colorframe, variable = self.color, value = "orange", text = "ORANGE")
-        self.radioorange.grid(row = 1, column = 1, padx = 5, pady = 5)
-        self.radioyellow = Radiobutton(self.colorframe, variable = self.color, value = "yellow", text = "YELLOW")
-        self.radioyellow.grid(row = 1, column = 2, padx = 5, pady = 5)
-        self.radiogreen = Radiobutton(self.colorframe, variable = self.color, value = "green", text = "GREEN")
-        self.radiogreen.grid(row = 1, column = 3, padx = 5, pady = 5)
-        self.radioblue = Radiobutton(self.colorframe, variable = self.color, value = "blue", text = "BLUE")
-        self.radioblue.grid(row = 1, column = 4, padx = 5, pady = 5)
-        self.radiopurple = Radiobutton(self.colorframe, variable = self.color, value = "purple", text = "PURPLE")
-        self.radiopurple.grid(row = 1, column = 5, padx = 5, pady = 5)
-        self.radiopink = Radiobutton(self.colorframe, variable = self.color, value = "pink", text = "PINK")
-        self.radiopink.grid(row = 1, column = 6, padx = 5, pady = 5)
+        Radiobutton(self.colorframe, variable = self.color, value = "red", text = "RED").grid(row = 1, column = 0, padx = 5, pady = 5)
+        Radiobutton(self.colorframe, variable = self.color, value = "orange", text = "ORANGE").grid(row = 1, column = 1, padx = 5, pady = 5)
+        Radiobutton(self.colorframe, variable = self.color, value = "yellow", text = "YELLOW").grid(row = 1, column = 2, padx = 5, pady = 5)
+        Radiobutton(self.colorframe, variable = self.color, value = "green", text = "GREEN").grid(row = 1, column = 3, padx = 5, pady = 5)
+        Radiobutton(self.colorframe, variable = self.color, value = "blue", text = "BLUE").grid(row = 1, column = 4, padx = 5, pady = 5)
+        Radiobutton(self.colorframe, variable = self.color, value = "purple", text = "PURPLE").grid(row = 1, column = 5, padx = 5, pady = 5)
+        Radiobutton(self.colorframe, variable = self.color, value = "pink", text = "PINK").grid(row = 1, column = 6, padx = 5, pady = 5)
 
-        # TODO: Check boxes for mood words
-        #Checkbutton(master, text="male", variable=var1).grid(row=0, sticky=W)
+        # Check boxes for mood words
+        Checkbutton(self.moodframe, text="manic", variable=self.manic).grid(row = 2, column = 1, padx = 5, pady = 5)
+        Checkbutton(self.moodframe, text="depressive", variable=self.depressive).grid(row = 2, column = 2, padx = 5, pady = 5)
+        Checkbutton(self.moodframe, text="overwhelmed", variable=self.overwhelmed).grid(row = 2, column = 3, padx = 5, pady = 5)
+        Checkbutton(self.moodframe, text="apathetic", variable=self.apathetic).grid(row = 2, column = 4, padx = 5, pady = 5)
+        Checkbutton(self.moodframe, text="happy", variable=self.happy).grid(row = 2, column = 5, padx = 5, pady = 5)
+        Checkbutton(self.moodframe, text="sad", variable=self.sad).grid(row = 3, column = 1, padx = 5, pady = 5)
+        Checkbutton(self.moodframe, text="irritable", variable=self.irritable).grid(row = 3, column = 2, padx = 5, pady = 5)
+        Checkbutton(self.moodframe, text="calm", variable=self.calm).grid(row = 3, column = 3, padx = 5, pady = 5, ipadx = 5)
+        Checkbutton(self.moodframe, text="fulfilled", variable=self.fulfilled).grid(row = 3, column = 4, padx = 5, pady = 5)
+        Checkbutton(self.moodframe, text="empty", variable=self.empty).grid(row = 3, column = 5, padx = 5, pady = 5)
+
 
         # Submit
-        self.Button = Button(self.frame, text = "Submit", command = submit)
-        self.Button.grid(row = 2, column = 0, sticky = E,  padx = 5, pady = 5)
+        self.Button = Button(self.frame, text = "Submit", command = submit).grid(row = 4, column = 0, sticky = E,  padx = 5, pady = 5)
 
 
 def main():
