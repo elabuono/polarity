37f173e75db3f06df3358666119ebd79d34e486e
Erica LaBuono <erica.labuono@gmail.com>
2022-02-13 20:14:28 -0500

    Basic color and layout
    Added question and answer functionality for color

--- polarity.py
+++ polarity.py
@@ -18,23 +18,38 @@ class Window:
                 csvwriter.writerow(row)
             self.master.destroy()
 
-        # Frame
-        self.frame = Frame(self.master, width = 400, height = 400)
-        self.frame.pack()
+        # Frames
+        self.frame = Frame(self.master, width = 300, height = 300)
+        self.frame.pack( padx = 10, pady = 10, side = LEFT)
+        self.colorframe = Frame(self.frame, width = 250, height = 250)
+        self.colorframe.grid(row = 1, column = 0, padx = 10, pady = 10, sticky = SW)
 
-        # Radiobutton
+        # Label
+        self.label = Label(self.frame, text="  What color is your mood?  ", font = "Helvetica 16 italic", relief=GROOVE)
+        self.label.grid(row=0, column=0,  padx = 10, pady = 10)
 
-        self.radiored = Radiobutton(self.frame, variable = self.color, value = "red", text = "RED")
-        self.radiored.grid(row = 0, column = 0, sticky = W, padx = 20, pady = 20)
+        # Radios/Colors
+        self.radiored = Radiobutton(self.colorframe, variable = self.color, value = "red", text = "RED")
+        self.radiored.grid(row = 1, column = 0, padx = 5, pady = 5)
+        self.radioorange = Radiobutton(self.colorframe, variable = self.color, value = "orange", text = "ORANGE")
+        self.radioorange.grid(row = 1, column = 1, padx = 5, pady = 5)
+        self.radioyellow = Radiobutton(self.colorframe, variable = self.color, value = "yellow", text = "YELLOW")
+        self.radioyellow.grid(row = 1, column = 2, padx = 5, pady = 5)
+        self.radiogreen = Radiobutton(self.colorframe, variable = self.color, value = "green", text = "GREEN")
+        self.radiogreen.grid(row = 1, column = 3, padx = 5, pady = 5)
+        self.radioblue = Radiobutton(self.colorframe, variable = self.color, value = "blue", text = "BLUE")
+        self.radioblue.grid(row = 1, column = 4, padx = 5, pady = 5)
+        self.radiopurple = Radiobutton(self.colorframe, variable = self.color, value = "purple", text = "PURPLE")
+        self.radiopurple.grid(row = 1, column = 5, padx = 5, pady = 5)
+        self.radiopink = Radiobutton(self.colorframe, variable = self.color, value = "pink", text = "PINK")
+        self.radiopink.grid(row = 1, column = 6, padx = 5, pady = 5)
 
-        self.radioblue = Radiobutton(self.frame, variable = self.color, value = "blue", text = "BLUE")
-        self.radioblue.grid(row = 0, column = 1, sticky = W, pady = 20)
+        # TODO: Check boxes for mood words
+        #Checkbutton(master, text="male", variable=var1).grid(row=0, sticky=W)
 
-        self.radiogreen = Radiobutton(self.frame, variable = self.color, value = "green", text = "GREEN")
-        self.radiogreen.grid(row = 0, column = 1, sticky = W, pady = 20)
-
+        # Submit
         self.Button = Button(self.frame, text = "Submit", command = submit)
-        self.Button.grid(row = 1, column = 1, sticky = E, padx = 20, pady = 20)
+        self.Button.grid(row = 2, column = 0, sticky = E,  padx = 5, pady = 5)
 
 
 def main():
