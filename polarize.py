56cb1462ec107ef08570ff31e16f8ec2cff0c42c
Erica LaBuono <erica.labuono@gmail.com>
2022-02-17 11:27:03 -0500

    Polarize Libraries
    using pandas to visualize table, plan to use pandas to create a new graph of color-word comparison

--- polarize.py
+++ polarize.py
@@ -1,6 +1,25 @@
+import numpy as np
+from pandastable import Table, TableModel
+from IPython.display import display
+import tkinter as tk
+
 # polarize data
-# we want to open csv and generate a graphic
-# first stage: open data and put words to each color
-# bigger number for more common word per color
-# ignore word if it isn't matched up
+# we want to open csv and generate a visual of the data collected
 # https://pandas.pydata.org/
+filepath = '2022.csv'
+
+root = tk.Tk()
+root.geometry('800x400')
+root.title('Polarity Data')
+
+class TestApp(tk.Frame):
+    def __init__(self, parent, filepath):
+        super().__init__(parent)
+        self.table = Table(self, showtoolbar=True, showstatusbar=True)
+        self.table.importCSV(filepath)
+        self.table.show()
+
+app = TestApp(root, filepath)
+app.pack(fill=tk.BOTH, expand=1)
+
+root.mainloop()
