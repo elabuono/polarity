2244e76a081df049c906bc99b132833d693f8c94
Erica LaBuono <erica.labuono@gmail.com>
2022-02-24 10:58:23 -0500

    polarity view pulls by month
    print/returns the month's data based on user input

--- polarity-view.py
+++ polarity-view.py
@@ -5,7 +5,7 @@ from tkinter import *
 
 
 class Window:
-    def __init__(self, master, csv_reader):
+    def __init__(self, master, csv_file):
         self.master = master
         
         dropValue = StringVar(master)
@@ -22,22 +22,29 @@ class Window:
         
         def submit():
             print ("value is: " + dropValue.get())
+            monthRequest = dropValue.get()
+            csv_reader = csv.reader(csv_file)
+            next(csv_reader) # to skip header in our file before processing data
             for row in csv_reader:
-                monthNumeric = row[0:1]
+                monthNumeric = row[0]
                 if monthNumeric != '':
-                    print(monthNumeric)
+                    month = (datetime.strptime(monthNumeric, "%m/%d/%Y %H:%M")).strftime("%B")
+                    if(month == monthRequest):
+                        print(row)
+            csv_file.seek(0)
             
         button = Button(master, text="SUBMIT", command=submit)
         button.pack()
 
 def main():
     # Open Data
-    csv_reader = csv.reader(open('2022.csv', 'r'))
+    csv_file = open('2022.csv', 'r')
     # Open app
     root = Tk()
     root.title("Polarity View")
-    window = Window(root, csv_reader)
+    window = Window(root, csv_file)
     root.mainloop()
+    csv_file.close()
   
     
 main()
